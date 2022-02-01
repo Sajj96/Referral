0e7eb9dc6f51f6d5bdbbfb829f45466ee62f09ac
Sajj96 <captainsajjad4@gmail.com>
2022-02-01 17:10:02 +0300

    Trivia Question

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -18,8 +18,8 @@
                             <h4>Video</h4>
                         </div>
                         <div class="card-body">
-                            <video id="my-video" class="video-js" controls preload="auto" poster="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg" data-setup='' loop>
-                                <source src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1080p.mp4" type='video/mp4'>
+                            <video id="my-video" class="video-js" controls preload="auto" poster="{{ asset('assets/img/image-gallery/thumb/thumb-1.png')}}" data-setup='' loop>
+                                <source src="{{ asset('assets/videos/sintel-short.mp4')}}" type='video/mp4'>
                             </video>
                         </div>
                     </div>
@@ -38,22 +38,22 @@
                         <div class="card-body">
                             <div id="aniimated-thumbnials" class="list-unstyled row clearfix">
                                 <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-                                    <a href="https://www.youtube.com/embed/xFa2_PVMeDQ?rel=0&autoplay=1" data-sub-html="Demo Description">
+                                    <a href="assets/img/image-gallery/thumb/thumb-2.png" data-sub-html="Demo Description">
                                         <img class="img-responsive thumbnail" src="assets/img/image-gallery/thumb/thumb-2.png" alt="">
                                     </a>
                                 </div>
                                 <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-                                    <a href="https://www.youtube.com/embed/hSrOpTYKNMw?rel=0&autoplay=1" data-sub-html="Demo Description">
+                                    <a href="assets/img/image-gallery/thumb/thumb-3.png" data-sub-html="Demo Description">
                                         <img class="img-responsive thumbnail" src="assets/img/image-gallery/thumb/thumb-3.png" alt="">
                                     </a>
                                 </div>
                                 <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-                                    <a href="assets/img/image-gallery/4.png" data-sub-html="Demo Description">
+                                    <a href="assets/img/image-gallery/thumb/thumb-4.png" data-sub-html="Demo Description">
                                         <img class="img-responsive thumbnail" src="assets/img/image-gallery/thumb/thumb-4.png" alt="">
                                     </a>
                                 </div>
                                 <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-                                    <a href="assets/img/image-gallery/5.png" data-sub-html="Demo Description">
+                                    <a href="assets/img/image-gallery/thumb/thumb-5.png" data-sub-html="Demo Description">
                                         <img class="img-responsive thumbnail" src="assets/img/image-gallery/thumb/thumb-5.png" alt="">
                                     </a>
                                 </div>
